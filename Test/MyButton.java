f728737b853674e7b6c1bca0344653d51b9e6302
362109188 <362109188@qq.com>
2020-10-27 14:07:41 +0800

    Create MyButton.java

---
+++ Test/MyButton.java
@@ -0,0 +1,16 @@
+package Test;
+
+//类3
+
+import javax.swing.JButton;
+
+/**
+* MyButton 按钮
+*
+*/
+public class MyButton extends JButton {
+	private static final long serialVersionUID = 1L;
+	//按钮坐标值
+	protected int row;
+	protected int col;
+}
